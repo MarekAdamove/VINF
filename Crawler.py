f9d9f6e5ec369a3d4e3c84b10bce6935fb755a5b
unknown <majo.adamove@gmail.com>
2023-10-25 14:02:40 +0200

    Fixed duplicates

--- Crawler.py
+++ Crawler.py
@@ -10,6 +10,7 @@ class Crawler:
         self.visited_urls = []
         self.unvisited_urls = [starting_url]
         self.num_pages_to_download = num_pages_to_download
+        self.used_titles = []
 
         self.crawl()
     def download_url(self, url):
@@ -18,19 +19,31 @@ class Crawler:
     def save_html(self, page):
         page_title_tag = re.search("<title.*>.*</title>", page.text, flags= re.IGNORECASE | re.MULTILINE)
         page_title = re.sub("<.*?>", "", page_title_tag.group())
-        page_title = re.sub(r'[\/*?:"<>|]', "", page_title)
+        page_title = re.sub(r'[/*?:"<>|]', "", page_title)
+
+        self.used_titles.append(page_title)
+
+        if page_title in self.used_titles:
+            page_title += f"{self.used_titles.count(page_title)+1}"
 
         with open(f"{self.SAVE_HTML_PATH}/{page_title}.html", "w", encoding="utf-8") as file:
-            file.write(page.text)
+            if file is None:
+                print("Error")
+            else:
+                file.write(page.text)
 
     def find_links(self, page):
-        link_pattern = r"<a\s+href\s*=\s*[\"\'](/[^\s]+)[\"\'].*>.*</a>"
+        link_pattern = r"<a\s+href\s*=\s*[\"\'](/[^\.\"\']+)[\"\'].*>.*</a>"
 
         return re.findall(link_pattern, page.text, flags= re.IGNORECASE | re.MULTILINE)
 
 
     def crawl(self):
         for relative_link in self.unvisited_urls:
+            if relative_link in self.visited_urls:
+                self.unvisited_urls.remove(relative_link)
+                continue
+
             if len(self.visited_urls) < self.num_pages_to_download:
                 response = self.download_url(self.ROOT_URL + relative_link)
                 self.visited_urls.append(relative_link)
@@ -40,7 +53,9 @@ class Crawler:
                 self.unvisited_urls += self.find_links(response)
 
                 print(f"[{len(self.visited_urls)}/{self.num_pages_to_download}] Page: {self.ROOT_URL}{relative_link} downloaded.")
+            else:
+                break
 
 
 
-crawler = Crawler("/wiki/World_of_Warcraft:_Dragonflight", 1000,"https://wowpedia.fandom.com")
+crawler = Crawler("/wiki/World_of_Warcraft:_Dragonflight", 5000,"https://wowpedia.fandom.com")
